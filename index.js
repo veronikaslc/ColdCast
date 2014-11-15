3a571ca5ec6bf02c7672751892c5f0df4c1dedfd
veronikaslc <veronika-koltunov@gmail.com>
2014-11-15 02:04:17 -0500

    All data comming to node back-end
    got all data from API for all canadian cities

--- index.js
+++ index.js
@@ -16,7 +16,33 @@ var API_key = '7efdf026c7705a541d3bdd32bb344712';
 
 var doc;
 
-request('http://api.openweathermap.org/data/2.5/weather?id='+toronto_code+'&APPID='+API_key, function(error, resp, body) {
+var cities_file  = 'city_list.txt';
+var cities = [];
+
+var fs = require('fs');
+var array = fs.readFileSync(cities_file).toString().split("\n");
+for(i in array) {
+    var str = array[i].split("\t");
+    if (str[4] == 'CA') {
+        cities.push({id: str[0], name: str[1], lat: str[2], lon: str[3]});
+    }
+}
+
+console.log(cities.length);
+//var test_url = 'http://api.openweathermap.org/data/2.5/weather?id='+toronto_code+'&APPID='+API_key;
+
+var id_list='';
+
+for (j in cities){
+    id_list = id_list + ',' + cities[j].id;
+}
+
+id_list = id_list.substring(1, id_list.length);
+//console.log(id_list);
+
+var real_url ='http://api.openweathermap.org/data/2.5/group?id=' + id_list +'&units=metric&APPID='+API_key;
+
+request(real_url, function(error, resp, body) {
    doc = body;
     console.log(body);
 });
@@ -24,5 +50,8 @@ request('http://api.openweathermap.org/data/2.5/weather?id='+toronto_code+'&APPI
 app.get('/getweather', function (req, resp) {
     resp.json(doc);
 });
+
+
+
 
 app.listen(3000);
